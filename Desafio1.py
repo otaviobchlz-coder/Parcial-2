8533ca4e39be76a1ce1eb6d68ca276dfa20c4bfc
otaviobchlz-coder <otavio.buchholz@escola.pr.gov.br>
2026-03-17 08:30:32 -0300

    Create Desafio1.py

---
+++ Desafio1.py
@@ -0,0 +1,12 @@
+def soma(a, b):
+    return a + b
+
+# Solicita ao usuário os dois números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma
+resultado = soma(num1, num2)
+
+# Exibe o resultado
+print(f"A soma de {num1} e {num2} é {resultado}")
